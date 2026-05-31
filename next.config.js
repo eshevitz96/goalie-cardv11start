78f06157d7d2f39a93001349cd769c451a427c4d
eshevitz96 <eshevitz96@gmail.com>
2026-05-31 13:15:43 -0400

    Re-enable output standalone to support Vercel Serverless Function size limit optimization

--- next.config.js
+++ next.config.js
@@ -1,20 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  experimental: {
-    outputFileTracingExcludes: {
-      '*': [
-        'node_modules/@supabase/cli-darwin-arm64/**/*',
-        'node_modules/@supabase/cli-linux-x64/**/*',
-        'node_modules/@supabase/cli-darwin-x64/**/*',
-        'node_modules/typescript/**/*',
-        'node_modules/eslint/**/*',
-        'ios/**/*',
-        'android/**/*',
-        'dist/**/*',
-      ],
-    },
-  },
+  output: 'standalone',
 }
 
 module.exports = nextConfig
