8222f880e82d51543c72309fa58c17f48247eadc
eshevitz96 <eshevitz96@gmail.com>
2026-05-31 13:31:05 -0400

    Remove heavy and unnecessary folders (ios, android, Goalie Card Dashboard) from git tracking

--- ios/App/App/ProximityReaderPlugin.m
+++
@@ -1,9 +0,0 @@
-#import <Foundation/Foundation.h>
-#import <Capacitor/Capacitor.h>
-
-// Define the plugin methods that will be accessible to JavaScript
-CAP_PLUGIN(ProximityReaderPlugin, "ProximityReader",
-           CAP_PLUGIN_METHOD(checkAvailability, CAPPluginReturnPromise);
-           CAP_PLUGIN_METHOD(startScanning, CAPPluginReturnPromise);
-           CAP_PLUGIN_METHOD(stopScanning, CAPPluginReturnPromise);
-)
