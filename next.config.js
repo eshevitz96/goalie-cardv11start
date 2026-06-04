43b0108510ba8af9a1d4de258dc6706f6a2cc285
eshevitz96 <eshevitz96@gmail.com>
2026-06-04 17:46:10 -0400

    config: update next.config.js to exclude build-size bloating folders

--- next.config.js
+++ next.config.js
@@ -1,5 +1,16 @@
+const path = require('path');
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  outputFileTracingRoot: path.join(__dirname, './'),
+  outputFileTracingExcludes: {
+    '*': [
+      'node_modules/vite/**/*',
+      'ios/**/*',
+      'android/**/*',
+      '.next/cache/**/*',
+    ],
+  },
 }
 module.exports = nextConfig
